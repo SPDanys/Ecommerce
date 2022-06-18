c79e9154a2b407219cc2539cea70bcace0e429f7
dany <denny.dany@gmail.com>
2022-06-18 16:14:13 +0530

    Arun has finished the work

--- src/com/ecommerce/AmazonSample.java
+++ src/com/ecommerce/AmazonSample.java
@@ -2,4 +2,11 @@ package com.ecommerce;
 
 public class AmazonSample {
 
+	private void arunSample() {
+		System.out.println("Arun is working");
+	}
+
+	public static void main(String[] args) {
+
+	}
 }
