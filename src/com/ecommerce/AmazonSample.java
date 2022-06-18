60a1463cc29127a0cf837c81e11c30180b1a021f
dany <denny.dany@gmail.com>
2022-06-18 17:03:32 +0530

    Arun Day 2 Finished

--- src/com/ecommerce/AmazonSample.java
+++ src/com/ecommerce/AmazonSample.java
@@ -4,6 +4,7 @@ public class AmazonSample {
 
 	private void arunSample() {
 		System.out.println("Arun is working");
+		System.out.println("Arun Day 2");
 	}
 
 	public static void main(String[] args) {
